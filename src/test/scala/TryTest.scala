edc2818e562d7af6ad61e5e94a8fe6d31ba75181
Naren David de Avila Julio - Ceiba Software <naren.deavila@ceiba.com.co>
2018-12-06 10:50:01 -0500

    Testing chaining operations of try

--- src/test/scala/TryTest.scala
+++ src/test/scala/TryTest.scala
@@ -19,6 +19,100 @@ class TryTest extends FunSuite {
     assert(Failure(UnderAgeException("The party is only for adults"))=== actual)
   }
 
+  test("checkingTryWithIsSuccess"){
 
+    val actual = Try(PartyTicketService.buyPartyTicket(Person("Naren",18)))
+
+    assert(actual.isSuccess)
+  }
+  test("checkingTryWithIsFailure"){
+    val actual = Try(PartyTicketService.buyPartyTicket(Person("Naren",16)))
+
+    assert(actual.isFailure)
+  }
+
+  test("simpleTryWithGetOrElse"){
+    val try01 = Try(PartyTicketService.buyPartyTicket(Person("Naren",16)))
+
+    val actual = try01 getOrElse PartyTicket("Street seller")
+
+    assert(PartyTicket("Street seller") === actual)
+  }
+
+  /*************************************************
+    Chaining operations(Mapping and flat mapping)
+    **********************************************/
+
+  test("usingMap"){
+
+    val actual = Try(PartyTicketService.buyPartyTicket(Person("Naren David", 20))).map(x => x.origin)
+
+    assert(Success("Official seller") === actual)
+  }
+
+  test("nestedTryStructureWithMap"){
+    val dividendString = "24";
+    val divisor = 6;
+
+    val actual = Try(Integer.parseInt(dividendString)).map(x => Try( x / divisor))
+
+    assert(Success(Success(4)) === actual)
+  }
+
+  test("nestedTryStructureWithFaltMap"){
+    val dividendString = "24";
+    val divisor = 6;
+
+    val actual = Try(Integer.parseInt(dividendString)).flatMap(x => Try( x / divisor))
+
+    assert(Success(4) === actual)
+  }
+
+  /*************************************************
+    Chaining operations(Filter)
+    **********************************************/
+
+  test("filteringATryThenSuccess"){
+
+    val actual = Try(PartyTicketService.buyPartyTicket(Person("Naren",18))).filter(x => x.origin.equals("Official seller"))
+
+    assert(Success(PartyTicket("Official seller")) === actual)
+  }
+
+  test("filteringATryThenFailure"){
+
+    val actual = Try(PartyTicketService.buyPartyTicket(Person("Naren",18))).filter(x => x.origin.equals("Unknown seller"))
+
+    assert(actual.isFailure)
+  }
+
+  /*************************************************
+    Pattern Matching
+    **********************************************/
+
+  test("simpleTryWithPatternMatchingThenSuccess"){
+
+    val try01 = Try(PartyTicketService.buyPartyTicket(Person("Naren",18)))
+
+    val actual: String = try01 match {
+      case Success(PartyTicket("Official seller")) => "Do something here!"
+      case Failure(UnderAgeException("The party is only for adults")) => "Do another thing here!"
+    }
+
+    assert("Do something here!" === actual)
+  }
+
+
+  test("simpleTryWithPatternMatchingThenFailure"){
+
+    val try01 = Try(PartyTicketService.buyPartyTicket(Person("Naren",14)))
+
+    val actual: String = try01 match {
+      case Success(PartyTicket("Official seller")) => "Do something here!"
+      case Failure(UnderAgeException("The party is only for adults")) => "Do another thing here!"
+    }
+
+    assert("Do another thing here!" === actual)
+  }
 
 }
